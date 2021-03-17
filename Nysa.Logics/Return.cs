613c3ad48351e7f0e1cc73d7fc08a376a64de0e1
Admin <Admin@CLOUD.ODYHOST.NET>
2021-03-17 16:31:07 -0500

    Changed return type of Try to Pending for easier use.

--- Nysa.Logics/Return.cs
+++ Nysa.Logics/Return.cs
@@ -16,8 +16,8 @@ namespace Nysa.Logics
         public static Lazy<T> Lazy<T>(this Func<T> @this, Boolean isThreadSafe = false)
             => new Lazy<T>(@this, isThreadSafe);
 
-        public static Try<T> Try<T>(this Func<T> @this)
-            => (Try<T>)@this;
+        public static Pending<T> Try<T>(this Func<T> @this)
+            => (Pending<T>)@this;
 
         public static Option<T> Some<T>(this T value)
             => new Some<T>(value);
