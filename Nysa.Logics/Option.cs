63cf3d4b11969884ce8043d30004febef5604081
slowsigma <slowsigma@hotmail.com>
2021-03-29 15:31:16 -0500

    Affect in Option replaces Send function.

--- Nysa.Logics/Option.cs
+++ Nysa.Logics/Option.cs
@@ -10,10 +10,23 @@ namespace Nysa.Logics
         public static readonly Option<T> None = new None<T>();
         public static implicit operator Option<T>(Option option) => Option<T>.None;
 
+        [Obsolete("Misleading name, use Affect instead.")]
         public Unit Send(Action<T> action)
         {
             if (this is Some<T> some)
                 action(some.Value);
+
+            return Unit.Value;
+        }
+
+        /// <summary>
+        /// Takes an Option of some type and calls an update
+        /// action if the Option is Some of that type.
+        /// </summary>
+        public Unit Affect(Action<T> update)
+        {
+            if (this is Some<T> some)
+                update(some.Value);
 
             return Unit.Value;
         }
