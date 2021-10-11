b8d93d42c4a59386424ac808fc9e3c4d0f34371d
slowsigma <slowsigma@hotmail.com>
2021-10-11 16:41:11 -0500

    Increase read width.

--- Nysa.Data.SqlClient/Read.cs
+++ Nysa.Data.SqlClient/Read.cs
@@ -139,4 +139,82 @@ namespace Nysa.Data.SqlClient
         public Func<SqlDataReader, TResult> Make<TResult>(Func<TA, TB, TC, TD, TE, TF, TG, TH, TI, TJ, TK, TResult> how) => r => how(A(r, 0), B(r, 1), C(r, 2), D(r, 3), E(r, 4), F(r, 5), G(r, 6), H(r, 7), I(r, 8), J(r, 9), K(r, 10));
     }
 
+    public class Read<TA, TB, TC, TD, TE, TF, TG, TH, TI, TJ, TK, TL>
+    {
+        private Get<TA> A;
+        private Get<TB> B;
+        private Get<TC> C;
+        private Get<TD> D;
+        private Get<TE> E;
+        private Get<TF> F;
+        private Get<TG> G;
+        private Get<TH> H;
+        private Get<TI> I;
+        private Get<TJ> J;
+        private Get<TK> K;
+        private Get<TL> L;
+        internal Read(Get<TA> a, Get<TB> b, Get<TC> c, Get<TD> d, Get<TE> e, Get<TF> f, Get<TG> g, Get<TH> h, Get<TI> i, Get<TJ> j, Get<TK> k, Get<TL> l) { A = a; B = b; C = c; D = d; E = e; F = f; G = g; H = h; I = i; J = j; K = k; L = l; }
+        public Func<SqlDataReader, TResult> Make<TResult>(Func<TA, TB, TC, TD, TE, TF, TG, TH, TI, TJ, TK, TL, TResult> how) => r => how(A(r, 0), B(r, 1), C(r, 2), D(r, 3), E(r, 4), F(r, 5), G(r, 6), H(r, 7), I(r, 8), J(r, 9), K(r, 10), L(r, 11));
+    }
+
+    public class Read<TA, TB, TC, TD, TE, TF, TG, TH, TI, TJ, TK, TL, TM>
+    {
+        private Get<TA> A;
+        private Get<TB> B;
+        private Get<TC> C;
+        private Get<TD> D;
+        private Get<TE> E;
+        private Get<TF> F;
+        private Get<TG> G;
+        private Get<TH> H;
+        private Get<TI> I;
+        private Get<TJ> J;
+        private Get<TK> K;
+        private Get<TL> L;
+        private Get<TM> M;
+        internal Read(Get<TA> a, Get<TB> b, Get<TC> c, Get<TD> d, Get<TE> e, Get<TF> f, Get<TG> g, Get<TH> h, Get<TI> i, Get<TJ> j, Get<TK> k, Get<TL> l, Get<TM> m) { A = a; B = b; C = c; D = d; E = e; F = f; G = g; H = h; I = i; J = j; K = k; L = l; M = m; }
+        public Func<SqlDataReader, TResult> Make<TResult>(Func<TA, TB, TC, TD, TE, TF, TG, TH, TI, TJ, TK, TL, TM, TResult> how) => r => how(A(r, 0), B(r, 1), C(r, 2), D(r, 3), E(r, 4), F(r, 5), G(r, 6), H(r, 7), I(r, 8), J(r, 9), K(r, 10), L(r, 11), M(r, 12));
+    }
+
+    public class Read<TA, TB, TC, TD, TE, TF, TG, TH, TI, TJ, TK, TL, TM, TN>
+    {
+        private Get<TA> A;
+        private Get<TB> B;
+        private Get<TC> C;
+        private Get<TD> D;
+        private Get<TE> E;
+        private Get<TF> F;
+        private Get<TG> G;
+        private Get<TH> H;
+        private Get<TI> I;
+        private Get<TJ> J;
+        private Get<TK> K;
+        private Get<TL> L;
+        private Get<TM> M;
+        private Get<TN> N;
+        internal Read(Get<TA> a, Get<TB> b, Get<TC> c, Get<TD> d, Get<TE> e, Get<TF> f, Get<TG> g, Get<TH> h, Get<TI> i, Get<TJ> j, Get<TK> k, Get<TL> l, Get<TM> m, Get<TN> n) { A = a; B = b; C = c; D = d; E = e; F = f; G = g; H = h; I = i; J = j; K = k; L = l; M = m; N = n; }
+        public Func<SqlDataReader, TResult> Make<TResult>(Func<TA, TB, TC, TD, TE, TF, TG, TH, TI, TJ, TK, TL, TM, TN, TResult> how) => r => how(A(r, 0), B(r, 1), C(r, 2), D(r, 3), E(r, 4), F(r, 5), G(r, 6), H(r, 7), I(r, 8), J(r, 9), K(r, 10), L(r, 11), M(r, 12), N(r, 13));
+    }
+
+    public class Read<TA, TB, TC, TD, TE, TF, TG, TH, TI, TJ, TK, TL, TM, TN, TO>
+    {
+        private Get<TA> A;
+        private Get<TB> B;
+        private Get<TC> C;
+        private Get<TD> D;
+        private Get<TE> E;
+        private Get<TF> F;
+        private Get<TG> G;
+        private Get<TH> H;
+        private Get<TI> I;
+        private Get<TJ> J;
+        private Get<TK> K;
+        private Get<TL> L;
+        private Get<TM> M;
+        private Get<TN> N;
+        private Get<TO> O;
+        internal Read(Get<TA> a, Get<TB> b, Get<TC> c, Get<TD> d, Get<TE> e, Get<TF> f, Get<TG> g, Get<TH> h, Get<TI> i, Get<TJ> j, Get<TK> k, Get<TL> l, Get<TM> m, Get<TN> n, Get<TO> o) { A = a; B = b; C = c; D = d; E = e; F = f; G = g; H = h; I = i; J = j; K = k; L = l; M = m; N = n; O = o; }
+        public Func<SqlDataReader, TResult> Make<TResult>(Func<TA, TB, TC, TD, TE, TF, TG, TH, TI, TJ, TK, TL, TM, TN, TO, TResult> how) => r => how(A(r, 0), B(r, 1), C(r, 2), D(r, 3), E(r, 4), F(r, 5), G(r, 6), H(r, 7), I(r, 8), J(r, 9), K(r, 10), L(r, 11), M(r, 12), N(r, 13), O(r, 14));
+    }
+
 }
