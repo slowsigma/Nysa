90601cd125c42e1940fe6e5806e59687324142b1
slowsigma <slowsigma@hotmail.com>
2022-02-11 14:38:18 -0600

    FirstToken function.

--- Nysa.CodeAnalysis.VbScript/Semantics/Code/CodeNodeExtensions.cs
+++ Nysa.CodeAnalysis.VbScript/Semantics/Code/CodeNodeExtensions.cs
@@ -12,13 +12,19 @@ namespace Nysa.CodeAnalysis.VbScript.Semantics
     public static class CodeNodeExtensions
     {
 
-        public static Option<(Token Start, Token End)> TokenBounds(this CodeNode @this)
+        public static (Token? Start, Token? End) TokenBounds(this CodeNode @this)
         {
             var tokens = @this.Node.Match(n => n.Where(nt => nt.IsToken).Select(v => v.AsToken).ToArray(), () => new Token[] {});
 
             return tokens.Length > 0
-                   ? (Start: tokens[0], End: tokens[tokens.Length - 1]).Some()
-                   : Option.None;
+                   ? (Start: tokens[0], End: tokens[tokens.Length - 1])
+                   : (Start: null, End: null);
+        }
+
+        public static Token? FirstToken(this CodeNode @this, Dorata.Text.Identifier tokenId)
+        {
+            return @this.Node.Match(n => n.Where(nt => nt.IsToken).Select(v => v.AsToken).FirstOrNone(tk => tk.Id.Equals(tokenId)).Match(f => f, () => (Token?)null),
+                                    () => (Token?)null);
         }
 
     }
