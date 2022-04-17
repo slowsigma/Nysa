947e947004dd8cb55a999e833f8f1639231e0c7d
slowsigma <slowsigma@hotmail.com>
2022-04-16 20:34:31 -0500

    More symbol meta data.

--- Nysa.CodeAnalysis.VbScript/Semantics/Symbols/SymbolLayer.cs
+++ Nysa.CodeAnalysis.VbScript/Semantics/Symbols/SymbolLayer.cs
@@ -8,17 +8,24 @@ using Nysa.Text;
 namespace Nysa.CodeAnalysis.VbScript.Semantics
 {
 
-    public record SymbolLayer
+    public record SymbolLayer : ISymbolScope
     {
+        private static IReadOnlySet<String> _NoTags = new HashSet<String>(StringComparer.OrdinalIgnoreCase);
+
         public Option<SymbolLayer>          Up       { get; init; }
         public ISymbolScope                 Value    { get; init; }
         public IReadOnlyList<Symbol>        Members  => this.Value.Members;
         public IDictionary<String, Symbol>  Index    => this.Value.Index;
+        public IReadOnlySet<String>         Tags     => this.Value.Tags;
+        public IReadOnlySet<String>         AssignedTags(String name) => this._AssignedTags.ContainsKey(name) ? this._AssignedTags[name] : _NoTags;
+
+        private Dictionary<String, HashSet<String>> _AssignedTags;
 
         public SymbolLayer(Option<SymbolLayer> up, ISymbolScope value)
         {
-            this.Up     = up;
-            this.Value  = value;
+            this.Up             = up;
+            this.Value          = value;
+            this._AssignedTags  = new Dictionary<String, HashSet<String>>(StringComparer.OrdinalIgnoreCase);
         }
 
         public Option<SymbolLayer> MemberLayer(String symbolName)
@@ -26,6 +33,19 @@ namespace Nysa.CodeAnalysis.VbScript.Semantics
                && this.Index[symbolName] is ISymbolScope scope
                ? (new SymbolLayer(this.Some(), scope)).Some()
                : Option<SymbolLayer>.None;
+
+        public SymbolLayer MemberLayer(ISymbolScope member)
+            => new SymbolLayer(this.Some(), member);
+
+        public Unit AssignTag(String name, String tag)
+        {
+            if (!this._AssignedTags.ContainsKey(name))
+                this._AssignedTags.Add(name, new HashSet<String>(StringComparer.OrdinalIgnoreCase));
+
+            this._AssignedTags[name].Add(tag);
+
+            return Unit.Value;
+        }
     }
 
 }
