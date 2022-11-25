e222aa96e16511446afcb57d3dfda5bde9d07a68
slowsigma <slowsigma@hotmail.com>
2022-11-25 15:56:08 -0600

    Performance fix.

--- Nysa.CodeAnalysis/Text/Parsing/GrammarRule.cs
+++ Nysa.CodeAnalysis/Text/Parsing/GrammarRule.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace Nysa.Text.Parsing;
 
-public sealed record GrammarRule
+public sealed class GrammarRule
 {
-    private SymbolIndex _Index  { get; init; }
-    public Identifier   Id      { get; init; }
-    public String       Symbol  => this._Index.Symbol(this.Id);
-    public IReadOnlyList<Identifier> DefinitionIds { get; init; }
-    public Boolean IsEmpty      => this.DefinitionIds.Count == 0;
+    private SymbolIndex                 _Index  { get; init; }
+    public Identifier                   Id      { get; init; }
+    public String                       Symbol  => this._Index.Symbol(this.Id);
+    public IReadOnlyList<Identifier>    DefinitionIds { get; init; }
+    public Boolean IsEmpty              => this.DefinitionIds.Count == 0;
 
     internal GrammarRule(SymbolIndex index, Identifier id, IEnumerable<Identifier> definitionIds)
     {
