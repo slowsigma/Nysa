bca3979c8fabdc2a96036a1cada19c1cf184fb54
Philip Fucich <slowsigma@hotmail.com>
2022-12-09 12:12:45 -0600

    Support optional argument symbols.

--- Nysa.CodeAnalysis.VbScript/Semantics/Symbols/ArgumentSymbol.cs
+++ Nysa.CodeAnalysis.VbScript/Semantics/Symbols/ArgumentSymbol.cs
@@ -10,29 +10,31 @@ namespace Nysa.CodeAnalysis.VbScript.Semantics
 
     public record ArgumentSymbol : TypedSymbol
     {
-        public Boolean IsByRef { get; private set; }
+        public Boolean IsByRef { get; init; }
+        public Boolean IsOptional { get; init; }
 
-        private ArgumentSymbol(String name, Option<String> newName, Option<String> message, Option<String> typeName, Boolean isByRef, IReadOnlySet<String> tags)
+        private ArgumentSymbol(String name, Option<String> newName, Option<String> message, Option<String> typeName, Boolean isByRef, Boolean isOptional, IReadOnlySet<String> tags)
             : base(name, newName, message, typeName, tags)
         {
-            this.IsByRef = isByRef;
+            this.IsByRef    = isByRef;
+            this.IsOptional = isOptional;
         }
 
-        public ArgumentSymbol(String name, Option<String> newName, Option<String> typeName, Boolean isByRef = true, params String[] tags)
-            : this(name, newName, Option.None, typeName, isByRef, new HashSet<String>(tags, StringComparer.OrdinalIgnoreCase))
+        public ArgumentSymbol(String name, Option<String> newName, Option<String> typeName, Boolean isByRef = true, Boolean isOptional = false, params String[] tags)
+            : this(name, newName, Option.None, typeName, isByRef, isOptional, new HashSet<String>(tags, StringComparer.OrdinalIgnoreCase))
         {
         }
 
-        public ArgumentSymbol(String name, Boolean isByRef = true, params String[] tags)
-            : this(name, Option.None, Option.None, Option.None, isByRef, new HashSet<String>(tags, StringComparer.OrdinalIgnoreCase))
+        public ArgumentSymbol(String name, Boolean isByRef = true, Boolean isOptional = false, params String[] tags)
+            : this(name, Option.None, Option.None, Option.None, isByRef, isOptional, new HashSet<String>(tags, StringComparer.OrdinalIgnoreCase))
         {
         }
 
         public ArgumentSymbol Renamed(String newName)
-            => new ArgumentSymbol(this.Name, newName.Some(), this.Message, this.TypeName, this.IsByRef, this.Tags);
+            => new ArgumentSymbol(this.Name, newName.Some(), this.Message, this.TypeName, this.IsByRef, this.IsOptional, this.Tags);
 
         public override ArgumentSymbol WithType(String typeName)
-            => new ArgumentSymbol(this.Name, this.NewName, this.Message, typeName.Some(), this.IsByRef, this.Tags);
+            => new ArgumentSymbol(this.Name, this.NewName, this.Message, typeName.Some(), this.IsByRef, this.IsOptional, this.Tags);
     }
 
 }
